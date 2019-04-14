30de4f0f32c035b9c85cda1bce851a2069ee1101
iukarpov <spangegoth@gmail.com>
2019-04-14 19:29:18 +0300

    Initial Commit

---
+++ SBSFlickr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SBSFlickr
+//
+//  Created by Igor on 14/04/2019.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
